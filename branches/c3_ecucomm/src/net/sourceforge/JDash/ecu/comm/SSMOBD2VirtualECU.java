66ec23ac43d7fca4d9991f02ebe9dfa564f0d3da
cryptor3 <cryptor3@b819195f-f332-0410-ba9c-ff226281b76a>
2008-04-04 05:52:03 +0000

    Work checkpoint.  More finetuning to ECU class structure.

--- branches/c3_ecucomm/src/net/sourceforge/JDash/ecu/comm/SSMOBD2VirtualECU.java
+++ branches/c3_ecucomm/src/net/sourceforge/JDash/ecu/comm/SSMOBD2VirtualECU.java
@@ -23,13 +23,16 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  ******************************************************/
 package net.sourceforge.JDash.ecu.comm;
 import java.io.*;
-import net.sourceforge.JDash.ecu.comm.SSMOBD2ProtocolHandler.SSMPacket;
+import java.util.List;
+import java.util.ArrayList;
+import net.sourceforge.JDash.ecu.comm.SSMOBD2ProtocolHandler.*;
+import net.sourceforge.JDash.ecu.param.ECUParameter;
 /**
  * Emulates an ECU by responding to packets
  * @author greg
  */
 public class SSMOBD2VirtualECU extends VirtualECU {
-	SSMOBD2ProtocolHandler ssmprotohandler;
+	SSMOBD2ProtocolHandler ssmprotohandler = null;
 	SSMOBD2VirtualECU() {
 		super();
 	}
@@ -39,6 +42,15 @@ public class SSMOBD2VirtualECU extends VirtualECU {
 		InputStream  is = emuport.getECUInputStream();
 		OutputStream os = emuport.getECUOutputStream();
 		ssmprotohandler = new SSMOBD2ProtocolHandler(emuport);
+		
+		int paramsPerPacket = 
+				SSMOBD2ProtocolHandler.SSMPacket.MAX_DATA_LENGTH / 
+				SSMOBD2ProtocolHandler.SSM_OBD2_ADDRESS_SIZE;
+		List<ECUParameter> packetParamList = new ArrayList<ECUParameter>();
+		((ArrayList)packetParamList).ensureCapacity(paramsPerPacket);
+		
+		
+		
 		do {
 			try 
 			{
@@ -47,6 +59,27 @@ public class SSMOBD2VirtualECU extends VirtualECU {
 				{
 					rxPacket = ssmprotohandler.receivePacket(0);
 					System.out.println("SSMOBD2VirtualECU received a packet");
+
+					// Load packetParamList with the list of parameters that we wish
+					// to query.
+					SSMOBD2ProtocolHandler.decodeECUParameterQueryPacket(
+							rxPacket, packetParamList);
+					
+					// Make up some values to return.
+					for (ECUParameter p : packetParamList ) {
+						p.setResult(0); // TODO: rand
+					}
+					txPacket = SSMOBD2ProtocolHandler.encodeECUParameterQueryResponsePacket(packetParamList);
+					/* Send the TX packet */
+
+	                // GN: apparently the ECU is supposed to echo back the request packet?
+	                rxPacket.write(os);
+					
+										
+					txPacket.write(os);
+					os.flush();
+					
+					
 				}
 			} 
 			catch (IOException e) 
